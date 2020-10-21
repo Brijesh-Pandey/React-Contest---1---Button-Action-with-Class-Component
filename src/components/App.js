a823495f4803fcf8ee4266fe00156a3f26410028
Brijesh Pandey <30575594+Brijesh-Pandey@users.noreply.github.com>
2020-10-21 20:26:41 +0530

    first fix

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,27 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state={
-			text:""
-		} 
-	}
-	handleClick=()=>{
-		this.setState({this.state.text:`Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy`})
-	};
-    render() {
-    	return(
-    		<div id="main">
-				<button id="click" onClick={this.handleClick}>click</button>
-				<p id="para">{this.state.text}</p>
-    		</div>
-    	);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    };
+  }
+  handleClick = () => {
+    this.setState({
+      text: `Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy`
+    });
+  };
+  render() {
+    return (
+      <div id="main">
+        <button id="click" onClick={this.handleClick}>
+          click
+        </button>
+        <p id="para">{this.state.text}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
