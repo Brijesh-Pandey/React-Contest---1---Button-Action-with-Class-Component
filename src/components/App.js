9fdffcb858f796c93eaed3d329bd0545be7cf38a
Brijesh Pandey <30575594+Brijesh-Pandey@users.noreply.github.com>
2020-10-21 20:32:02 +0530

    working

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,27 @@
 import React, { Component, useState } from "react";
-import "../styles/App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      text: ""
-    };
+    this.state = { para: "" };
   }
-  handleClick = () => {
-    this.setState({
-      text: `Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy`
-    });
-  };
   render() {
     return (
       <div id="main">
-        <button id="click" onClick={this.handleClick}>
-          click
+        {/* Do not remove this main div!! */}
+        <button
+          id="click"
+          onClick={() =>
+            this.setState({
+              para:
+                "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+            })
+          }
+        >
+          Button
         </button>
-        <p id="para">{this.state.text}</p>
+        {this.state.para ? <p>{this.state.para}</p> : ""}
+        {/* <p id="para">{this.state.para}</p> */}
       </div>
     );
   }
